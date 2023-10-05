aab0a953f65dc82217bc160f695aa78ab14e9765
Giiid <gideonfummey1@gmail.com>
2023-10-05 15:31:27 +0200

    A model to add songs

--- admin.py
+++ admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Authentication
+from .models import Authentication, Song
 # Register your models here.
 admin.site.register(Authentication)
+admin.site.register(Song)
--- models.py
+++ models.py
@@ -7,3 +7,12 @@ class Authentication(models.Model):
 
     def __str__(self):
         return self.username
+
+class Song(models.Model):
+    artiste = models.CharField(max_length=150)
+    song_name = models.CharField(max_length=150)
+    cover_image = models.ImageField(upload_to='cover_images')
+    audio_file = models.FileField(upload_to='audios')
+
+    def __str__(self):
+        return self.artiste + '-' + self.song_name
